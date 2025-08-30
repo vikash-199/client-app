f4852a09e5e58a8006bfa1862e36cc89723e814e
vikash kumar <vikashkumar24041997@gmail.com>
2025-08-30 22:17:11 +0530

    craete a post comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/PostList.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+function PostList() {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/posts");
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+}
+
+export default PostList;
